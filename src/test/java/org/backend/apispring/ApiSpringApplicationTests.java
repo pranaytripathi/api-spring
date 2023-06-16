64d7c9f03b9b9b2cb7321d14ae58385b8e199a31
Pranay <pranaytripathi123@gmail.com>
2023-06-16 20:26:19 +0100

    Initial commit

---
+++ src/test/java/org/backend/apispring/ApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.backend.apispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
